b49c186a1c2b96c9db4271e685fae5ba4e158085
Renzo Maggiori <renzo.maggiori@epitech.eu>
2024-01-30 11:54:18 +0100

    [add] binary tree

---
+++ binaryTree/test.c
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2023
+** data-structure-c
+** File description:
+** test.c
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "tree.h"
+
+Test(binary_tree, test_createNode) {
+    TreeNode *node = createNode(10);
+    cr_assert_not_null(node, "createNode() should return a non-null pointer");
+    cr_assert_eq(node->data, 10, "Node data should be 10");
+    cr_assert_null(node->left, "Left child should be NULL");
+    cr_assert_null(node->right, "Right child should be NULL");
+    freeTree(node);
+}
+
+Test(binary_tree, test_insertNode) {
+    TreeNode *root = NULL;
+    root = insertNode(root, 15);
+    cr_assert_not_null(root, "insertNode() should not return NULL for first insert");
+    cr_assert_eq(root->data, 15, "Root node data should be 15");
+    freeTree(root);
+}
+
+Test(binary_tree, test_deleteNode) {
+    TreeNode *root = NULL;
+    root = insertNode(root, 20);
+    root = insertNode(root, 10);
+    root = deleteNode(root, 10);
+    cr_assert_not_null(root, "Root should not be NULL after delete");
+    TreeNode *searchResult = search(root, 10);
+    cr_assert_null(searchResult, "Deleted node should not be found");
+    freeTree(root);
+}
+
+Test(binary_tree, test_search) {
+    TreeNode *root = NULL;
+    root = insertNode(root, 25);
+    TreeNode *searchResult = search(root, 25);
+    cr_assert_not_null(searchResult, "Search should find the inserted node");
+    freeTree(root);
+}
---
+++ binaryTree/tree.c
@@ -0,0 +1,78 @@
+/*
+** EPITECH PROJECT, 2023
+** data-structure-c
+** File description:
+** tree.c
+*/
+
+#include <stdio.h>
+#include "tree.h"
+
+TreeNode* createNode(int data) {
+
+}
+
+TreeNode* insertNode(TreeNode* root, int data) {
+
+}
+
+TreeNode* deleteNode(TreeNode* root, int data) {
+
+}
+
+TreeNode* search(TreeNode* root, int data) {
+
+}
+
+void inorderTraversal(TreeNode* root) {
+
+}
+
+void preorderTraversal(TreeNode* root) {
+
+}
+
+void postorderTraversal(TreeNode* root) {
+
+}
+
+void freeTree(TreeNode* root) {
+
+}
+
+int main() {
+    TreeNode *root = NULL;
+
+    root = insertNode(root, 50);
+    insertNode(root, 30);
+    insertNode(root, 20);
+    insertNode(root, 40);
+    insertNode(root, 70);
+    insertNode(root, 60);
+    insertNode(root, 80);
+
+    printf("Inorder traversal of the given tree:\n");
+    inorderTraversal(root);
+
+    printf("\nPreorder traversal of the given tree:\n");
+    preorderTraversal(root);
+
+    printf("\nPostorder traversal of the given tree:\n");
+    postorderTraversal(root);
+
+    printf("\nDelete 20\n");
+    root = deleteNode(root, 20);
+    printf("Inorder traversal after deleting 20:\n");
+    inorderTraversal(root);
+
+    TreeNode *searchResult = search(root, 30);
+    if (searchResult != NULL) {
+        printf("\nElement 30 found in the tree\n");
+    } else {
+        printf("\nElement 30 not found in the tree\n");
+    }
+
+    freeTree(root);
+
+    return 0;
+}
